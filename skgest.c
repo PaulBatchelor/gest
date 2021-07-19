ef831d172e8c8eff2138c3f87d920a8c6212dc49
Paul Batchelor <thisispaulbatchelor@gmail.com>
2021-07-19 19:05:30 -0400

    refactored sknodes to have more patchwerk-neutral components

--- skgest.c
+++ skgest.c
@@ -84,14 +84,43 @@ static void destroy(pw_node *node)
     pw_memory_free(patch, &ud);
 }
 
+int pw_node_gesticulate(pw_node *node, gest_d *g)
+{
+    int rc;
+    pw_patch *patch;
+    struct gest_n *gest;
+    void *ud;
+
+    rc = pw_node_get_patch(node, &patch);
+    if (rc != PW_OK) return rc;
+
+    rc = pw_memory_alloc(patch, sizeof(struct gest_n), &ud);
+    SK_PW_ERROR_CHECK(rc);
+    gest = (struct gest_n *)ud;
+
+    rc = pw_node_cables_alloc(node, 2);
+
+
+    if (rc != PW_OK) return rc;
+
+    pw_node_set_block(node, 1);
+
+    pw_node_get_cable(node, 0, &gest->conductor);
+    pw_node_get_cable(node, 1, &gest->out);
+    gest->gest = g;
+
+    pw_node_set_data(node, gest);
+    pw_node_set_compute(node, compute);
+    pw_node_set_destroy(node, destroy);
+    return PW_OK;
+}
+
 int sk_node_gesticulate(sk_core *core)
 {
     pw_patch *patch;
     pw_node *node;
     int rc;
     sk_param conductor;
-    void *ud;
-    struct gest_n *gest;
     gest_d *g;
 
     rc = sk_param_get(core, &conductor);
@@ -102,48 +131,29 @@ int sk_node_gesticulate(sk_core *core)
 
     patch = sk_core_patch(core);
 
-    rc = pw_memory_alloc(patch, sizeof(struct gest_n), &ud);
-    SK_PW_ERROR_CHECK(rc);
-    gest = (struct gest_n *)ud;
-
-    gest->gest = g;
-
     rc = pw_patch_new_node(patch, &node);
     SK_PW_ERROR_CHECK(rc);
 
-    rc = pw_node_cables_alloc(node, 2);
+    rc = pw_node_gesticulate(node, g);
     SK_PW_ERROR_CHECK(rc);
-
-    pw_node_set_block(node, 1);
-
-    pw_node_get_cable(node, 0, &gest->conductor);
-    pw_node_get_cable(node, 1, &gest->out);
-
-    pw_node_set_data(node, gest);
-    pw_node_set_compute(node, compute);
-    pw_node_set_destroy(node, destroy);
 
     sk_param_set(core, node, &conductor, 0);
     sk_param_out(core, node, 1);
     return 0;
 }
 
-int sk_node_gestweight(sk_core *core)
+int pw_node_gestweight(pw_node *node, gest_d *g)
 {
-    pw_patch *patch;
-    pw_node *node;
-    int rc;
-    void *ud;
     struct gestweight_n *gw;
-    gest_d *g;
+    void *ud;
+    int rc;
+    pw_patch *patch;
 
-    rc = sk_core_generic_pop(core, (void **)&g);
-    SK_ERROR_CHECK(rc);
-
-    patch = sk_core_patch(core);
+    rc = pw_node_get_patch(node, &patch);
+    if (rc != PW_OK) return rc;
 
     rc = pw_memory_alloc(patch, sizeof(struct gest_n), &ud);
-    SK_PW_ERROR_CHECK(rc);
+    if (rc != PW_OK) return rc;
     gw = (struct gestweight_n *)ud;
 
     gw->gest = g;
@@ -153,11 +163,8 @@ int sk_node_gestweight(sk_core *core)
     gw->scale = 1.0 / 120.0;
     gw->prev = 0;
 
-    rc = pw_patch_new_node(patch, &node);
-    SK_PW_ERROR_CHECK(rc);
-
     rc = pw_node_cables_alloc(node, 1);
-    SK_PW_ERROR_CHECK(rc);
+    if (rc != PW_OK) return rc;
 
     pw_node_set_block(node, 0);
 
@@ -166,6 +173,28 @@ int sk_node_gestweight(sk_core *core)
     pw_node_set_data(node, gw);
     pw_node_set_compute(node, compute_weight);
     pw_node_set_destroy(node, destroy);
+
+
+    return PW_OK;
+}
+
+int sk_node_gestweight(sk_core *core)
+{
+    pw_patch *patch;
+    pw_node *node;
+    int rc;
+    gest_d *g;
+
+    rc = sk_core_generic_pop(core, (void **)&g);
+    SK_ERROR_CHECK(rc);
+
+    patch = sk_core_patch(core);
+
+    rc = pw_patch_new_node(patch, &node);
+    SK_PW_ERROR_CHECK(rc);
+
+    rc = pw_node_gestweight(node, g);
+    SK_PW_ERROR_CHECK(rc);
 
     sk_param_out(core, node, 0);
     return 0;
